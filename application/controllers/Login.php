7d657abb61d6514c0a884bb9c33aa6f875e81dd7
Albertus Adhi <albertus.usa69@gmail>
2016-10-26 19:57:17 +0700

    First commit

---
+++ application/controllers/Login.php
@@ -0,0 +1,53 @@
+<?php
+class Login extends CI_Controller {
+
+        public function index()
+        {	
+			$this->load->helper('form');
+			$this->load->library('form_validation');
+			$data = [];
+			$data['username'] = '';
+			$data['password'] = '';
+			$data['rmbMe'] = '';
+			
+			$this->load->view('header');
+			
+			if($this->input->post('loginBtn') == true){
+				$this->form_validation->set_rules('username','Wrong username','required');
+				$this->form_validation->set_rules('password','Wrong password','required|min_length[4]|max_length[8]');
+				
+				if($this->form_validation->run() == true)
+				{
+					//to check if the validation run correctly
+					//$this->load->view('welcome_message');
+
+					$data['username'] = $this->input->post('username');
+					$data['password'] = $this->input->post('password');
+					$data['rmbMe'] = $this->input->post('rememberChk');
+					
+					redirect('Home');
+					//$value = $this->m_login->login($username,$password,$rememberChk);
+
+					// if($value)
+					// {
+						// redirect('welcome_message');
+						// //return true;
+					// }
+					// else
+					// {
+						// $this->form_validation->set_message('login', 'password salah');
+						// redirect('c_login',$login); //i want to pass $login into login form, then print
+						// return false;               //them as a form_error
+
+					// }
+				}else{
+					echo '<div class="alert alert-danger" style="position:absolute;width:100%;top:0;">
+								<strong>Error!</strong> Username or Password wrong. Please try again.
+						
+						</div>';
+				}
+			}
+			$this->load->view('login', $data);
+			$this->load->view('footer');
+        }
+}
---
+++ application/views/login.php
@@ -0,0 +1,64 @@
+<head>
+<link href='http://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
+<link href="<?php echo base_url("assets/css/ext/login.css"); ?>" rel="stylesheet">
+</head>
+<body>
+<div class="middlePage">
+<div class="page-header">
+  <h1 class="logo">MECO <small>Welcome to our place!</small></h1>
+</div>
+
+<div class="panel panel-info">
+  <div class="panel-heading">
+    <h3 class="panel-title">Please Sign In</h3>
+  </div>
+  <div class="panel-body">
+  
+  <div class="row">
+  
+<div class="col-md-5" >
+<a href="#"><img src="http://techulus.com/buttons/fb.png" /></a><br/>
+<a href="#"><img src="http://techulus.com/buttons/tw.png" /></a><br/>
+<a href="#"><img src="http://techulus.com/buttons/gplus.png" /></a>
+</div>
+
+    <div class="col-md-7" style="border-left:1px solid #ccc;height:160px">
+	
+	<?php 
+		echo form_open("Login");
+			echo form_input("username",$username,"placeholder='Enter User Name' class='form-control input-md'");
+			echo '<div class="spacing"></div>';
+			echo form_input("password",$password,"placeholder='Enter Password' class='form-control input-md'");
+			echo '<div class="spacing"></div>';
+			echo form_checkbox("rememberChk", "Remember me", FALSE); echo "<small>Remember me</small>";
+			echo '<a href="#"><small style="float:right;"> Forgot Password?</small></a><br>';
+			echo form_submit('loginBtn', 'Sign In', 'class="btn btn-info btn-sm pull-right"');
+			
+		echo form_close();
+	?>
+	
+<!--
+	<fieldset>
+
+	  <input id="textinput" name="textinput" type="text" placeholder="Enter User Name" class="form-control input-md">
+	  <div class="spacing"></div>
+	  <input id="textinput" name="textinput" type="text" placeholder="Enter Password" class="form-control input-md">
+	  <div class="spacing">
+	  <input type="checkbox" name="checkboxes" id="checkboxes-0" value="1"><small> Remember me</small>
+	  <br/></div>
+	  <button id="singlebutton" name="singlebutton" class="btn btn-info btn-sm pull-right">Sign In</button>
+
+
+	</fieldset>
+-->
+</div>
+    
+</div>
+    
+</div>
+</div>
+
+<p><a href="#">About</a> &copy; MECO</p>
+
+</div>
+</body>
