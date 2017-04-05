cb1352cf6330b0fe2964c2f5482c4d3b87eb94ec
Albertus Adhi <albertus.usa69@gmail>
2017-04-05 15:40:29 +0700

    v1.2
    - adding several pages in dashboard
    - API for uploading json file
    - database integrated
    - configuring Data Tables page
    - fix DbTables functions
    - Charts fixed

---
+++ application/controllers/Charts.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by albertus
+ * Project MultiE-Comm
+ * on Mar 2017.
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Charts extends CI_Controller {
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('DbReport','',TRUE);
+    }
+    public function index()
+    {
+        $session_id = $this->session->userdata('loginData');
+        $data = $this->DbReport->getChartsData($session_id["ID_user"]);
+//        $data = $this->DbReport->getReportDataMonthly();
+
+        $response['data'] = $data;
+//        echo "<pre>";
+////        var_dump($session_id["ID_user"]) ;
+//        print_r($data);
+//        echo "</pre>";
+        $this->load->view('charts', $response);
+    }
+    function Weekly()
+    {
+        // weekly report here
+        $data = $this->DbReport->getReportDataMonthly();
+
+        $response['data'] = $data;
+        $this->load->view('charts', $response);
+    }
+}
